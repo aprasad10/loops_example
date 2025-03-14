789e553586a5aa753ddb9e5970b19c5a886b891e
aprasad10 <aprasad10@my.smccd.edu>
2025-03-14 05:38:12 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int[][] votesTable =
+                { {12, 13, 14, 10, 15, 16}, // candidate 0
+                        { 5,  2,  6,  8,  4,  3}, // candidate 1
+                        {13, 15, 10, 12, 11, 17}, // candidate 2
+                };
+        int voteCount;
+        System.out.println("Table length: " + votesTable.length);
+        for (int row[] : votesTable) {
+            voteCount = 0;
+            for (int vote : row) {
+                voteCount += vote;
+                }
+            System.out.println("Votes: " + voteCount);
+        }
+    }
+}
